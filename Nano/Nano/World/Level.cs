6c55a2470594109cfb42e5ef4ad8e99b4df53b46
Dykam <kramieb@gmail.com>
2013-04-17 21:14:05 +0200

    Added pathfinding integration.

--- Nano/Nano/World/Level.cs
+++ Nano/Nano/World/Level.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Engine.GameObjects;
+using PathFinding;
 
 namespace Nano.World
 {
 	class Level : GameObject
 	{
-		public Level()
+		public Map<Tile> Map { get; private set; }
+		public Level(int width, int height)
 			: base("level")
 		{
-
+			Map = new Map<Tile>(width, height);
 		}
 	}
 }
---
+++ Nano/Nano/World/Tile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PathFinding;
+
+namespace Nano.World
+{
+	class Tile : Node
+	{
+		public Tile()
+		{
+
+		}
+	}
+}
