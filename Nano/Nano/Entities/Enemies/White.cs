aa9ae8f84d4f61805dce49c242650c85248a6843
Dykam <kramieb@gmail.com>
2013-04-19 02:41:54 +0200

    Smarter pathfinding

--- Nano/Nano/Entities/Enemies/White.cs
+++ Nano/Nano/Entities/Enemies/White.cs
@@ -17,7 +17,8 @@ namespace Nano.Entities.Enemies
 		public override void Update(GameTime gameTime)
 		{
 			if (gameTime.TotalGameTime - LastSearch > TimeSpan.FromSeconds(1) && Vector2.DistanceSquared(State.Player.Transform.LocalPosition, this.Transform.LocalPosition) < 10 * 10) {
-				BuildPath(new Int2((int)State.Player.Transform.LocalPosition.X, (int)State.Player.Transform.LocalPosition.Y), gameTime);
+				var target = State.Player.Transform.LocalPosition + Vector2.One * .5f;
+				BuildPath(new Int2((int)target.X, (int)target.Y), gameTime);
 			}
 			base.Update(gameTime);
 			foreach (var skill in DNA.SkillCooling.Where(kvp => kvp.Value <= TimeSpan.Zero).ToArray()) {
