e0ed60bf85a19ca6440241c2b8f9abc25cad9528
Melleth <siemenkraayenbrink@gmail.com>
2013-04-19 03:10:20 +0200

    Added tutorial system.

--- Nano/Nano/Interface/TutorialBox.cs
+++ Nano/Nano/Interface/TutorialBox.cs
@@ -7,37 +7,98 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
 using Engine;
 using Nano;
+using Microsoft.Xna.Framework.Input;
 
 namespace Nano.Interface
 {
     class TutorialBox : GameObject
     {
         public bool Active;
+        int activeTutorial;
         Button closeButton;
+        SpriteFont font;
+        string tutorialMessage;
+        Dictionary<int, bool> tutorialTracker;
+        Dictionary<int, string> tutorials;
         public TutorialBox()
             : base()
         {
             Texture = NanoGame.Engine.ResourceManager.GetSprite("Sprites/tutorialTexture");
-            Active = true;
-            TileSheet tileSheet = new TileSheet(NanoGame.Engine.ResourceManager.GetSprite("MenuSheet"), 128);
-            closeButton = new Button(tileSheet, 1, 1, () => Console.WriteLine("click"))
-            {
-                Border = Color.Black,
-                Inner = Color.White
-            };
-            closeButton.Transform.LocalScale = new Vector2(0.1f,0.1f);
-            closeButton.Transform.Position = new Vector2(200, 375);
+            font = NanoGame.Engine.ResourceManager.GetFont("Fonts/Tutorial");
+            InitializeTutorials();
+            New(0);
         }
 
+        private void InitializeTutorials()
+        {
+            tutorials = new Dictionary<int, string>();
+            tutorialTracker = new Dictionary<int, bool>();
+            tutorials.Add(0, "Use the [W, A, S, D] keys to move around!"); tutorialTracker.Add(0, false);
+            tutorials.Add(1, "Press the left mouse button to shoot!"); tutorialTracker.Add(1, false);
+            tutorials.Add(2, "Press the spacebar to use Shockwave!"); tutorialTracker.Add(2, false);
+        }
+
+        public void New(int id)
+        {
+            activeTutorial = id;
+            tutorialMessage = tutorials[id];
+            Active = true;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            CheckForCompletion();
+
+        }
+
+        private void CheckForCompletion()
+        {
+            for (int i = 0; i < tutorialTracker.Count; i++)
+            {
+                if (!tutorialTracker[i])
+                {
+                    activeTutorial = i;
+                    i = 999;
+                }
+            }
+
+            switch (activeTutorial)
+            {
+                case 0:
+                    if (NanoGame.Engine.InputHelper.KeyPressed(Keys.A) || NanoGame.Engine.InputHelper.KeyPressed(Keys.S) || NanoGame.Engine.InputHelper.KeyPressed(Keys.D) || NanoGame.Engine.InputHelper.KeyPressed(Keys.W))
+                    {
+                        activeTutorial = 1;
+                        tutorialTracker[0] = true;
+                    }
+                    break;
+                case 1:
+                    if (NanoGame.Engine.InputHelper.MouseLeftButtonPressed())
+                    {
+                        activeTutorial = 2;
+                        tutorialTracker[1] = true;
+                    }
+                    break;
+                case 2:
+                    if (NanoGame.Engine.InputHelper.KeyPressed(Keys.Space))
+                    {
+                        tutorialTracker[2] = true;
+                    }
+                    break;
+            }
+            for (int i = 0; i < tutorialTracker.Count; i++)
+            {
+                if (tutorialTracker[i] && i == tutorialTracker.Count - 1)
+                    Active = false;
+            }
+                
+        }
 
         public override void Draw(SpriteBatch spriteBatch, Matrix transform)
         {
             if (Active)
             {
                 spriteBatch.Draw(Texture, Vector2.Zero, Color.White);
-                spriteBatch.End();
-                closeButton.Draw(spriteBatch, transform);
-                
+                spriteBatch.DrawString(font, Engine.HelperMethods.WrapText(font, tutorials[activeTutorial], 230), new Vector2(10,10), Color.Black);
             }
         }
     }
