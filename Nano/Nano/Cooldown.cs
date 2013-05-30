d648d6b959d6543199973ddbd724710a34721a47
Dykam <kramieb@gmail.com>
2013-05-30 17:26:17 +0200

    Added a cooldown utility class.

---
+++ Nano/Nano/Cooldown.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Nano
+{
+	class Cooldown
+	{
+		TimeSpan nextTick;
+		public TimeSpan Time { get; private set; }
+		public Cooldown(TimeSpan time)
+		{
+			Time = time;
+		}
+		public Cooldown(double seconds)
+			: this(TimeSpan.FromSeconds(seconds))
+		{
+		}
+
+		public bool TryTick(GameTime gameTime)
+		{
+			if (gameTime.TotalGameTime < nextTick)
+				return false;
+			nextTick = gameTime.TotalGameTime + Time;
+			return true;
+		}
+	}
+}
